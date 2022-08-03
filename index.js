220cf1deb7097e023452c42673319a3faf89f026
Adryel Simon <adryel@live.it>
2022-08-03 01:11:22 -0300

    feat: styles bootstrap icons

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 html = `
 <div class="container">    
-<h1>Alchemistic Speech</h1>
-<p>RECONHECIMENTO DE VOZ</p>
+<img src="mysticat.gif">
+<p>Transforme sua voz em texto</p>
 <textarea id="textarea"></textarea>
+<div class="options">
+<button id="btnGravar"> <i class="bi bi-mic"></i> </button>
+<button id="btnParar"> <i class="bi bi-mic-mute"></i> </button>
+<button id="btnBaixar"> <i class="bi bi-download"></i> </button>
+<button id="btnLimpar"> <i class="bi bi-trash"></i> </button>
 </div>
-<div class="options"></div>
+</div>
 `;
+
 document.getElementById("app").innerHTML = html;
