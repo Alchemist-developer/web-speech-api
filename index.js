64027fb03736ba99197696504de7f3ff6430eb37
Adryel Simon <adryel@live.it>
2022-08-03 02:01:05 -0300

    feat: stop icon button

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 html = `
 <div class="container">    
-<img src="mysticat.gif">
-<p>Transforme sua voz em texto</p>
-<textarea id="textarea"></textarea>
+<img src="/assets/mysticat.gif">
+<p>transforme sua voz em um arquivo de texto</p>
+<textarea placeholder="conte sua historia e baixe o arquivo 🐈" id="textarea"></textarea>
 <div class="options">
-<button id="btnGravar"> <i class="bi bi-mic"></i> </button>
-<button id="btnParar"> <i class="bi bi-mic-mute"></i> </button>
-<button id="btnBaixar"> <i class="bi bi-download"></i> </button>
-<button id="btnLimpar"> <i class="bi bi-trash"></i> </button>
+<button id="btnGravar" title="Iniciar"> <i class="bi bi-mic"></i> </button>
+<button id="btnParar" title="Parar"> <i class="bi bi-stop-circle"></i> </button>
+<button id="btnBaixar" title="Baixar"> <i class="bi bi-download"></i> </button>
+<button id="btnLimpar" title="Limpar"> <i class="bi bi-trash"></i> </button>
 </div>
 </div>
 `;
