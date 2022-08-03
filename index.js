d439ea0b5275ddc29d7c447dd62cdae229de589b
Adryel Simon <adryel@live.it>
2022-08-02 23:58:35 -0300

    feat: init proj using js

---
+++ index.js
@@ -0,0 +1,9 @@
+html = `
+<div class="container">    
+<h1>Alchemistic Speech</h1>
+<p>RECONHECIMENTO DE VOZ</p>
+<textarea id="textarea"></textarea>
+</div>
+<div class="options"></div>
+`;
+document.getElementById("app").innerHTML = html;
