b0b127aad3e263992593ced68c227e6095557a14
Adryel Simon <adryel@live.it>
2022-08-03 01:10:43 -0300

    feat: init speech

---
+++ speech.js
@@ -0,0 +1,5 @@
+const textarea = document.querySelector('#textarea');
+const btnGravar = document.querySelector('#btnGravar');
+const btnParar= document.querySelector('#btnParar');
+const btnBaixar = document.querySelector('#btnBaixar');
+const btnLimpar = document.querySelector('#btnLimpar');
